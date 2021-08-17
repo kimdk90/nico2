e80a1fe48add839c606362a2b15432e9f71034d5
kimdk90 <ahskxk@naver.com>
2021-08-17 18:01:19 +0900

    nico2 2021.08.17

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to server O");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message:", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Connected to server X");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,13 +1,36 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
-app.use("public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req,res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
   
 const handleListen = () => console.log(`listenign on http://localhost:3000`);
-app.listen(3000, handleListen); 
+//app.listen(3000, handleListen);
+
+//http,ws 같이 실행하기
+const server = http.createServer(app); //http
+
+const wss = new WebSocket.Server({server}); //ws
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser X");
+    });
+
+    socket.on("message", (message) => {
+        console.log(message);
+    });
+    
+    socket.send("hello!!!");
+});
+
+server.listen(3000, handleListen);
